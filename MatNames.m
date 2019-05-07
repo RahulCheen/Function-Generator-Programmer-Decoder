dd14e7d4d274b332bbd5464a2ad39fbf2f4eef79
achaarju <achaarju@users.noreply.github.com>
2019-05-07 09:24:58 -0600

    add raw data file name to outputs

--- MatNames.m
+++ MatNames.m
@@ -1,11 +1,11 @@
-function [DataName,ExtractedDataName,ParameterOrderName] = MatNames
+function [DataName,ExtractedDataName,ParameterOrderName,RawDataName] = MatNames
 
 [rawFileName,path1] = uigetfile('*.rhs','Select Raw File','MultiSelect','Off');
 
 DataName           = [rawFileName(1:end-4),'.mat'];
 ExtractedDataName  = ['trials_', rawFileName(1:end-4),'.mat'];
 ParameterOrderName = ['ParameterOrder_',rawFileName(1:end-4),'.mat'];
-
+RawDataName = rawFileName;
 cd(path1);
 
 
