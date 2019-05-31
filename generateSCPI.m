8adfb7978df842d9ded6228598fc144c222d63db
achaarju <achaarju@users.noreply.github.com>
2019-05-31 11:50:33 -0600

    Update generateSCPI.m

--- generateSCPI.m
+++ generateSCPI.m
@@ -1,22 +1,41 @@
 clearvars -except FG
 
-pulseDuration = 250; %[ms]
-arbWave = DCWithRiseAndFall(1000,30);
-arbWaveDAC = round(arbWave*32767);
+% parameters
+ARB_npoints = 1000;     % [# of points]
+ARB_risetime = 30;      % [% of total waveform]
+pulseDuration = 250;    % [ms]
+
+% generate waveform
+arbWave = DCWithRiseAndFall(ARB_npoints,ARB_risetime);  % generates waveform
+arbWaveDAC = round(arbWave*(2^15 - 1));                 % change to DAC values, signed, 16-bit
+
 ARBname = 'DC_with_rise';
 SEQname = 'PulseOnTrig';
-try fclose(FG);
+
+% need to increase buffer size to send in larger ARB data points
+
+try fclose(FG); % variable exists, in open state
     FG.OutputBufferSize = 32768;
     fopen(FG);
 catch
+    if ~exist('FG','var') % variable doesnt exist
+        FG = visa('keysight','USB0::0x0957::0x2A07::MY52600694::0::INSTR')
+        FG.OutputBufferSize = 32768;
+        % This address depends on the serial number of the machine.
+        fopen(FG) % There's some output here, so you know it worked.
+    
+    else % already closed
+        FG.OutputBufferSize = 32768;
+        fopen(FG);
+    end
 end
 
-if ~exist('FG','var')
-    FG = visa('keysight','USB0::0x0957::0x2A07::MY52600694::0::INSTR')
-    FG.OutputBufferSize = 32768;
-    % This address depends on the serial number of the machine.
-    fopen(FG) % There's some output here, so you know it worked.
-end
+% if ~exist('FG','var')
+%     FG = visa('keysight','USB0::0x0957::0x2A07::MY52600694::0::INSTR')
+%     FG.OutputBufferSize = 32768;
+%     % This address depends on the serial number of the machine.
+%     fopen(FG) % There's some output here, so you know it worked.
+% end
 
 % header + parameters
 fprintf(FG,'DATA:VOL:CLE');
@@ -24,18 +43,35 @@ fprintf(FG,'SOUR1:FUNC ARB'); % Change channel 1's waveform to ARB
 fprintf(FG,['SOUR1:FUNC:ARB:SRATE ',num2str(length(arbWave)*1000/pulseDuration)]);
 fprintf(FG,'SOUR1:FUNC:ARB:FILTER STEP');
 fprintf(FG,'SOUR1:FUNC:ARB:PTPEAK 2');
+fprintf(FG,'SOUR2:AM SOUR CH1');
+fprintf(FG,'SOUR2:AM:DEPT 100');
+fprintf(FG,'SOUR2:AM:DSSC ON');
+fprintf(FG,'SOUR2:AM:STAT ON');
 
 % generate ARB in function generator
 datastr = sprintf(',%4.3f',arbWave);
 fprintf(FG,['DATA:ARB:DAC ',ARBname,sprintf(',%d',arbWaveDAC)]);
-fprintf(FG,['FUNC:ARB ',ARBname]);
+%fprintf(FG,['FUNC:ARB ',ARBname]);
 
 % create SEQ in function generator
-fprintf(FG,'DATA:ARB DC0,0,0,0,0,0,0,0,0,0,0,0');
+fprintf(FG,'DATA:ARB DC0,0,0,0,0,0,0,0,0,0,0,0'); % zero volts to start
 
-command0 = ['"',SEQname,'","DC0",1,repeatTilTrig,maintain,4,"',ARBname,'",1,once,maintain,4,"DC0",1,once,maintain,4'];
-npoints = length(command0)-1;
-ndigits = floor(log10(length(command0)))+1;
-fprintf(FG,['DATA:SEQ #',num2str(ndigits),num2str(npoints),command0]);
-fprintf(FG,['FUNC:ARB ',SEQname]);
+    % sequence goes:
+    %   1) 0 volts,             repeat until trigger
+    %   2) arbitrary waveform,  once
+    %   3) 0 volts,             once
+SEQ_command = ['"',SEQname,'",',...
+    '"DC0",         1,repeatTilTrig,maintain,4,',... % 0 volts, repeat until trigger
+    '"',ARBname,'", 1,once,         maintain,4,',... % arbitrary waveform, once
+    '"DC0",         1,once,         maintain,4'];    % 0 volts, repeat once
+npoints = length(SEQ_command)-1;
+ndigits = floor(log10(length(SEQ_command)))+1;
+
+    % send to function generator
+fprintf(FG,['DATA:SEQ #',...
+    num2str(ndigits),... % number of digits in block
+    num2str(npoints),... % number of points in block
+    SEQ_command]);       % actual sequence order
+
+fprintf(FG,['FUNC:ARB ',SEQname]); % change to sequence
 
