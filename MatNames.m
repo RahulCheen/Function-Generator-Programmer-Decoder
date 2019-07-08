ba71811aee4c5a22da520e2e17fc5801ab2af06e
achaarju <achaarju@users.noreply.github.com>
2019-07-08 15:37:36 -0600

    changes to file conversions/read-in

--- MatNames.m
+++ MatNames.m
@@ -1,12 +1,13 @@
-function [DataName,ExtractedDataName,ParameterOrderName,RawDataName] = MatNames
+function [DigitalName,TrialsName,AmpName,AnalogName,RawDataName] = MatNames(path)
 
-[rawFileName,path1] = uigetfile('*.rhs','Select Raw File','MultiSelect','Off');
+[rawFileName,path1] = uigetfile([path,'*.rhs'],'Select Raw File','MultiSelect','Off');
 
-DataName           = [rawFileName(1:end-4),'.mat'];
-ExtractedDataName  = ['trials_', rawFileName(1:end-4),'.mat'];
-ParameterOrderName = ['ParameterOrder_',rawFileName(1:end-4),'.mat'];
-RawDataName = rawFileName;
-cd(path1);
+DigitalName         = [rawFileName(1:end-4),'_Digital.mat'  ];
+AmpName             = [rawFileName(1:end-4),'_Amplifier.mat'];
+AnalogName          = [rawFileName(1:end-4),'_Analog.mat'   ];
 
+TrialsName          = ['trials_', rawFileName(1:end-4),'.mat'];
+RawDataName         = rawFileName;
+cd(path1);    
 
 end
